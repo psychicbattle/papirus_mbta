5437695a47f1e042c1d289547917cb1475d97385
C. Allen <comradechris@gmail.com>
2021-03-07 20:32:58 -0500

    Dynamically determine destination name; better support for non-PaPirus writes; better caching before write to PaPirus, with font sizing / spacing.  Trimmed strings where possible.

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import pandas as pd
 import logging
 try:
     from papirus import PapirusTextPos
+    PAPIRUS_ENABLED = True
 except:
-    pass
-MBTA_URL = "https://api-v3.mbta.com/predictions"
+    logging.error("No Papirus found! Will output to stdout.")
+    PAPIRUS_ENABLED = False
+
+MBTA_URL = "https://api-v3.mbta.com/"
 
 try:
     API_KEY = os.environ["MBTA_API_KEY"]
@@ -14,8 +17,14 @@ except:
     logging.warning("No API key - continuing without. Rate limit will be greatly stricter")
     API_KEY = None
 
-STOP_ID = 2546
-ROUTE_ID = 86
+
+#List of queries (stop ID, route ID):
+ROUTES = [(2546, 86), #Washington & Beacon to Sullivan
+          (2570, 86)] #Washington & Beacon to Reservoir
+
+FONT_SIZE=17
+Y_SHIFT=16
+#These configs allow for 5 lines of text on screen and 16 characters
 
 try:
     papirusText = PapirusTextPos(rotation=0)
@@ -27,22 +36,30 @@ def strfdelta(tdelta):
     d["hours"], rem = divmod(tdelta.seconds, 3600)
     d["minutes"], d["seconds"] = divmod(rem, 60)
     if d["hours"] > 0:
-        return f"{d['hours']} hours, {d['minutes']} min"
+        return f"{d['hours']}h{d['minutes']}m"
     else:
-        return f"{d['minutes']} min"
+        return f"{d['minutes']}m"
     #return fmt.format(**d)
 
 
-def simple_test():
+def query(stop_id, route_id):
     """Just query something!"""
     #rt = pymbta3.Stops(key=API_KEY)
     headers = {"accept": "application/vnd.api+json"}
     if API_KEY:
         headers["x-api-key"] = API_KEY
-    r = requests.get(url=MBTA_URL,params={"filter[stop]":STOP_ID,
-                                      "filter[route]":ROUTE_ID}, headers=headers)
-    json = r.json()
-    first_arrival = json["data"][0]["attributes"]["arrival_time"]
+
+    #Get route destinations
+    r = requests.get(url=MBTA_URL+f"/routes/{route_id}", headers=headers)
+    route = r.json()
+    destinations = route["data"]["attributes"]["direction_destinations"]
+    #Trim destinations to the first word.
+    destinations = [x.split(" ")[0] for x in destinations]
+    r = requests.get(url=MBTA_URL+"/predictions",params={"filter[stop]":stop_id,
+                                      "filter[route]":route_id}, headers=headers)
+    predictions = r.json()
+    first_arrival = predictions["data"][0]["attributes"]["arrival_time"]
+    direction = predictions["data"][0]["attributes"]["direction_id"]
     first_arrival = pd.Timestamp(first_arrival)
     now = pd.Timestamp("now")
     now = now.tz_localize(first_arrival.tz)
@@ -50,13 +67,23 @@ def simple_test():
         time_str = "Now"
     else:
         time_str = strfdelta(first_arrival - now)
-    try:
-        send_to_papirus("86 to Sullivan: " + time_str)
-    except:
-        pass
+    return(f"{route_id} to {destinations[direction]}: {time_str}")
 
-def send_to_papirus(str):
-    papirusText.write(str)
+def send_to_papirus(str_set):
+    y = 0
+    papirusText.Clear()
+    for str in str_set:
+        papirusText.AddText(str, 0, y, size=FONT_SIZE)
+        y += Y_SHIFT
+    papirusText.WriteAll()
+
 
 if __name__=="__main__":
-    simple_test()
+    display_strs = []
+    for stop_id, route_id in ROUTES:
+        display_strs.append(query(stop_id, route_id))
+    if PAPIRUS_ENABLED:
+        send_to_papirus(display_strs)
+    else:
+        for str in display_strs:
+            print(str, len(str))
